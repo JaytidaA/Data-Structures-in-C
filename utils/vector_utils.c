17b272ef6843eb73b8c5270ef20ee82ba93d885a
JaytidaA <ajoil4068@gmail.com>
2025-08-19 19:02:50 +0530

    Initial commit :3 (Moving files from other repo over here)

---
+++ utils/vector_utils.c
@@ -0,0 +1,11 @@
+/*
+ * vector_utils.c
+ * Defines utility functions and MACROS for Vectors defined in the
+ * repository https://github.com/JaytidaA/coding-cpp
+ * Author: https://github.com/JaytidaA/
+ */
+
+#include "vector/vector.h"
+#include "vector/vector_utils.h"
+
+/* Empty file as the function definition has been turned into a macro in vector_utils.h */
